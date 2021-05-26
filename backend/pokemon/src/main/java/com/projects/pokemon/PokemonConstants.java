a63f26790b033daf484b8e28dfe0f5ec46395de1
JuanGQCadavid <jquirozcadavid@gmail.com>
2021-05-25 19:41:45 -0500

    Error handling

--- backend/pokemon/src/main/java/com/projects/pokemon/PokemonConstants.java
+++ backend/pokemon/src/main/java/com/projects/pokemon/PokemonConstants.java
@@ -4,4 +4,7 @@ public final class PokemonConstants {
 
     public static final String DEFAULT_CONTROLLER_GET_START_VALUE = "0";
     public static final String DEFAULT_CONTROLLER_GET_LIMIT_VALUE = "10";
+
+    // Error Handler constants
+    public static final String MISSING_FIELDS = "MissingFieldsBody";
 }
---
+++ backend/pokemon/src/main/java/com/projects/pokemon/exception/MissingValuesException.java
@@ -0,0 +1,16 @@
+package com.projects.pokemon.exception;
+
+import org.springframework.http.HttpStatus;
+
+import static com.projects.pokemon.PokemonConstants.MISSING_FIELDS;
+
+public class MissingValuesException extends GeneralException {
+    public MissingValuesException(Object Payload) {
+        super( GeneralExceptionParams.builder()
+                .messageException("There is a problem with the fields format.")
+                .errorLabel(MISSING_FIELDS)
+                .status(HttpStatus.BAD_REQUEST)
+                .payload(Payload)
+                .build());
+    }
+}
